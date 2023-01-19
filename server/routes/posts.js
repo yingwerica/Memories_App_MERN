3777b06e24594c73c916e5cd50351ea3fc10ea3b
yingwerica <yingw.pro@gmail.com>
2023-01-19 14:57:49 -0700

    finished like post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,15 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js' // in Node, we should add.js to the end of files, but not in React
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js' // in Node, we should add.js to the end of files, but not in React
 
 const router = express.Router();
 
 // all the routs here are with prefix '/posts' added from index.js
 router.get('/', getPosts)
 router.post('/', createPost)
-router.patch('/:id', updatePost)
+router.patch('/:id', updatePost) //patch is for updating
 router.delete('/:id', deletePost)
-
+router.patch('/:id/likePost', likePost) //like the post is updating the number of the likes
 
 
 export default router;
