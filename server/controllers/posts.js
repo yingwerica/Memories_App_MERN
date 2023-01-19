51b872714b0b31ee4bf1efcb86b2b7d05052f573
yingwerica <yingw.pro@gmail.com>
2023-01-19 00:30:43 -0700

    edit post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,7 @@
+
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
+
 
 export const getPosts = async (req, res) => { 
     try {
@@ -18,4 +21,17 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,14 @@
 import express from 'express';
 
-import { getPosts, createPost } from '../controllers/posts.js' // in Node, we should add.js to the end of files, but not in React
+import { getPosts, createPost, updatePost } from '../controllers/posts.js' // in Node, we should add.js to the end of files, but not in React
 
 const router = express.Router();
 
 // all the routs here are with prefix '/posts' added from index.js
 router.get('/', getPosts)
 router.post('/', createPost)
-router.get('/', getPosts)
-router.get('/', getPosts)
-router.get('/', getPosts)
+router.patch('/:id', updatePost)
+
 
 
 export default router;
