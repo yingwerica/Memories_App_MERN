fae7e49f6d91462569e782bfc4a8489818017d87
yingwerica <yingw.pro@gmail.com>
2023-01-19 12:16:55 -0700

    Delete post finished

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost } from '../controllers/posts.js' // in Node, we should add.js to the end of files, but not in React
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js' // in Node, we should add.js to the end of files, but not in React
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', getPosts)
 router.post('/', createPost)
 router.patch('/:id', updatePost)
+router.delete('/:id', deletePost)
 
 
 
