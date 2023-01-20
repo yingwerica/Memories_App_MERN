b7e0ce0ada0e031e72631fedb3ba19399d639377
yingwerica <yingw.pro@gmail.com>
2023-01-19 19:01:51 -0700

    env and constants

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 //import routes
 import postRoutes from './routes/posts.js';
 
 //initial app that we can use all the methods
 const app = express();
+dotenv.config();
 
 //set up bodyParser to send request properly.
 app.use(bodyParser.json({limit: "30mb", extended: true})); //make sure when we send image, send with limited size.
@@ -17,12 +19,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-//connect to mongoDB
-const MONGO_URI='mongodb+srv://Yingw:erica2db@cluster0.y06paec.mongodb.net/memories?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
 
+
+const PORT = process.env.PORT || 5000;
+//connect to database
 mongoose.set('strictQuery', true); //remove deprication warnings
-mongoose.connect(MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
